651c0b6de7c54c58a4c0255a5e8040dedb503d70
Amit1198911 <amitahirwar1198911@gmail.com>
2025-02-03 20:07:42 +0530

    http_methods

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,36 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Middleware
 app.use(express.json()); // To parse JSON data
 
 // Enable CORS for all routes
-app.use(cors({
-  origin: 'https://full-stack-task-management-app-lake.vercel.app', // Allow requests from this origin
-  credentials: true, // Allow cookies and credentials
-}));
+app.use(
+  cors({
+    origin: "https://full-stack-task-management-app-lake.vercel.app", // Allow requests from this origin
+    credentials: true, // Allow cookies and credentials
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/restaurant";
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/restaurant';
-
-mongoose.connect(MONGO_URI,)
-.then(() => console.log('MongoDB connected successfully'))
-.catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/auth', require('./routes/auth.routes'));
-app.use('/api/menu', require('./routes/menu.routes'));
-app.use('/api/order', require('./routes/order.routes'));
-
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/menu", require("./routes/menu.routes"));
+app.use("/api/order", require("./routes/order.routes"));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
